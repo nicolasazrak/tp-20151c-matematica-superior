cba21f2b4e7b4c6b8db2b115de9e499c39bf6535
leoxs22 <leoxs22@users.noreply.github.com>
2015-06-16 20:23:21 -0300

    agregado un comment :P

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ for(i=0;i<=4;i++){
                 for(k=j+1;k<=6;k++){
                         for(l=k+1;l<=7;l++){
                                 var posibilidad = [muestras[i], muestras[j], muestras[k], muestras[l]];
+                                //Yo en vez de pushear y mandar en un array, evaluaría todo directo acá, asi no itera de más :P
                                 posibilidades.push(posibilidad);
                         }
                 }
